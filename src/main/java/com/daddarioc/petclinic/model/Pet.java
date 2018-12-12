8d2f985be0e5f3c3947e25f2db268f09de90b067
DADDC003 <christopher.daddario@espn.com>
2018-12-12 15:38:52 -0500

    add pojo models

---
+++ src/main/java/com/daddarioc/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package com.daddarioc.petclinic.model;
+
+import java.time.LocalDateTime;
+
+public class Pet {
+    private PetType type;
+    private Owner owner;
+    private LocalDateTime birthDate;
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+}
