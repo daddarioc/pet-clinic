93e8f165d11a1d808591a08d73e2585bfb104e56
DADDC003 <christopher.daddario@espn.com>
2018-12-27 14:32:54 -0500

    closes #23 changed AbstractMapService to extend Long and BaseEntity added private method to generate a key for the map for new entries modified the other service maps to match

--- pet-clinic-web/src/main/java/com/daddarioc/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/daddarioc/petclinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Chris");
         owner1.setLastName("D'Addario");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Holly");
         owner2.setLastName("D'Addario");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("John");
         vet1.setLastName("Smith");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Zach");
         vet2.setLastName("Orange");
 
