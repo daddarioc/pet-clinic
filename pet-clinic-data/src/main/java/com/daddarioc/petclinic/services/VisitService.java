7c5a960b592ebb98ab7258aec8af48d97f540b4a
DADDC003 <christopher.daddario@espn.com>
2019-01-09 16:23:24 -0500

    renamed map services to align added VisitMapService closes #46

---
+++ pet-clinic-data/src/main/java/com/daddarioc/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.daddarioc.petclinic.services;
+
+import com.daddarioc.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/daddarioc/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.daddarioc.petclinic.services.map;
+
+import com.daddarioc.petclinic.model.Visit;
+import com.daddarioc.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        // do basic validation then hand off to superclass to save
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
