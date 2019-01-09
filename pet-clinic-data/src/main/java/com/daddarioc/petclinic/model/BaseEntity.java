c2142df3fe8cc89e4fb02d5c1385aae45ab1c98f
DADDC003 <christopher.daddario@espn.com>
2019-01-09 13:52:59 -0500

    updated base entity and server port closes #35

--- pet-clinic-data/src/main/java/com/daddarioc/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/daddarioc/petclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package com.daddarioc.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass   // establishes this as a base class to JPA; this SPECIFIC class doesn't need to be mapped to the database
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
